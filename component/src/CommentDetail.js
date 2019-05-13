c071eb9141e57b7263f4e1c59a50e680a682c9fc
Jin Kwon <jin.kwon@xy.company>
2019-05-13 11:06:50 -0700

    done

--- component/src/CommentDetail.js
+++ component/src/CommentDetail.js
@@ -2,7 +2,8 @@ import React from 'react';
 import faker from 'faker';
 
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+    console.log(props);
     return (
         <div className="comment">
                 <a href="/" className="avatar">
@@ -10,12 +11,13 @@ const CommentDetail = () => {
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Sam
+                        {props.author} by {props.userName}
                     </a>
                     <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
+                        <span className="date">{props.timeAgo}</span>
                     </div>
-                    <div className="text">Nice blog post!</div>
+                    <div className="text">{props.content}</div>
+                    
                 </div>
             </div>
     );
--- component/src/index.js
+++ component/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
@@ -9,7 +10,24 @@ const App = () => {
                 Using a component, you will need the following syntax below
                 However, Javascript variable will use {} 
             */}
-            <CommentDetail /> 
+            <ApprovalCard>
+                Are you sure you want to do this?
+                <div>
+                    <h4>WARNING!</h4>
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author ="Sam" userName="sam123"
+                    timeAgo ="Today at 4:45PM" content="This is my first comment"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author ="Alex" userName="tempid"
+                    timeAgo = "Today at 2:00PM" content="This is my second made up comment"/>
+            </ApprovalCard>    
+            <ApprovalCard>
+                <CommentDetail author ="Jane" userName="temporaryUserName"
+                    timeAgo = "Yesterday at 5:00PM" content="Last comment, BOOM!"/> 
+            </ApprovalCard>
         </div>
     )
 };
